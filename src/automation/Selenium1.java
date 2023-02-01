7811dac84677b06d400bfeb0db6a548498b4018a
SeleniumS <sugy301198@gmail.com>
2023-02-01 21:13:45 +0530

    Added maximze

--- src/automation/Selenium1.java
+++ src/automation/Selenium1.java
@@ -9,6 +9,7 @@ public class Selenium1 {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\ACER\\Downloads\\New folder\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
 		driver.quit();
 		
 		
