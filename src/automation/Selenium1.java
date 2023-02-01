4b59f70d849a1dc1ca860b8d8ea2203253bd0242
SeleniumS <sugy301198@gmail.com>
2023-02-01 21:01:39 +0530

    Launch Url

--- src/automation/Selenium1.java
+++ src/automation/Selenium1.java
@@ -1,9 +1,17 @@
 package automation;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Selenium1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\ACER\\Downloads\\New folder\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.quit();
+		
+		
 
 	}
 
